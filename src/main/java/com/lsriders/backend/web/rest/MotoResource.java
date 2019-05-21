6ddd1bd649194e4a6649948fa4899b5b93a02b3c
FerranSalvat <ferry96@hotmail.es>
2019-05-21 20:27:21 +0200

    #3 Query Moto per id de usuari, i return de my-motos

--- src/main/java/com/lsriders/backend/web/rest/MotoResource.java
+++ src/main/java/com/lsriders/backend/web/rest/MotoResource.java
@@ -11,6 +11,7 @@ import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -120,6 +121,7 @@ public class MotoResource {
 
 
     @GetMapping("/motos/by-userextid/{id}")
+    @Transactional
     public List<Moto> getMotoById(@PathVariable Long id) {
         log.debug("REST request to get Moto : {}");
         User user = userExtRepository.findById(id).get().getUser();
